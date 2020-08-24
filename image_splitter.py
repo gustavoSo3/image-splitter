0adb448acd4890e704aa4d374ecefb0784a6fe39
GustavoSo3 <gustavonline.games@gmail.com>
2020-08-24 09:44:57 -0500

    Saving takes place on same directory as Original

--- image_splitter.py
+++ image_splitter.py
@@ -1,10 +1,15 @@
 import sys
+import os
 from PIL import Image 
 
 filePath = ""
+pathToFile = ""
+fileExtention = ""
+fileName = ""
+
 widthDivisions = 3
 heightDivisions = 1
-fileExtention = ""
+
 
 for i in range(len(sys.argv)):
   #Asking for help command?
@@ -23,15 +28,32 @@ for i in range(len(sys.argv)):
 
   if sys.argv[i] == "-p":
     filePath = str(sys.argv[i+1])
-
+    
 if not filePath == "":
+
+  pathArgsList = filePath.split("/")
+  if len(pathArgsList) >= 1:
+    for i in range(len(pathArgsList)-1):
+      pathToFile += str(pathArgsList[i]) + "/"
+
+
   fileExtention = filePath.split(".")[-1]
+  fileName = filePath.split("/")[-1].split(".")[0]
   try:
       img = Image.open(filePath)
       width, height = img.size
 
       sizeW = (int)(width/widthDivisions)
       sizeH = (int)(height/heightDivisions)
+    
+      nTry = 1
+      finalPath = pathToFile + fileName
+
+      while os.path.exists(finalPath):
+        finalPath = pathToFile + fileName + "(" + str(nTry) + ")"
+        nTry += 1
+      
+      os.makedirs(finalPath)
       
       for w_i in range(widthDivisions):
         for h_i in range(heightDivisions):
@@ -42,7 +64,8 @@ if not filePath == "":
           actualCrop = img.crop(actualArea)
           
           newURL = str(w_i)+','+str(h_i)+'.'+str(fileExtention)
-          actualCrop.save(newURL)
+          print(finalPath + "/" + newURL)
+          actualCrop.save(finalPath + "/" + newURL)
 
   except IOError:
     pass
